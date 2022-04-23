70ee3e8eb94f2f7a67e2155adc46a59e94228835
Natasha Beddow <beddow90@gmail.com>
2022-04-23 17:22:03 +0100

    first commit, added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Declare generate button variable
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function to generate password
+function generatePassword() {
+  //prompt for password criteria
+  //prompt 1 - input password length (between 8 and 128 characters)
+  //if length under 8 or above 128 is selected, the user is asked again to choose the correct amount
+
+  //prompt 2 - user is asked whether to include lowercase, uppercase, numeric, and/or special characters
+  // Ensure at least one character type has been selected, otherwise request user to select again
+  //
+}
+
+// On click of generate button, this function is called
+// Function to write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
